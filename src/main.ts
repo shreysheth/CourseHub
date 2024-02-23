68f1779c64d457593af0e15310afa70b304ae6aa
shreysheth <shreysheth6487@gmail.com>
2024-02-24 01:53:22 +0530

    App Reconfig

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
@@ -6,11 +5,3 @@ import { AppModule } from './app/app.module';
 
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err));
-=======
-import { bootstrapApplication } from '@angular/platform-browser';
-import { appConfig } from './app/app.config';
-import { AppComponent } from './app/app.component';
-
-bootstrapApplication(AppComponent, appConfig)
-  .catch((err) => console.error(err));
->>>>>>> a415045eda77ba917fb44a7ecaf29f06cfbef71a
